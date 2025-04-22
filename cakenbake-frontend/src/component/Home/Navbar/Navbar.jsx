8c91ca58c18f48613e7f866e431657ac0a515a9c
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-04-22 23:20:46 +0530

    feat: add menu button to Navbar for small screens with conditional rendering

--- cakenbake-frontend/src/component/Home/Navbar/Navbar.jsx
+++ cakenbake-frontend/src/component/Home/Navbar/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import { IconButton, useMediaQuery } from '@mui/material';
+import { IoMenu } from "react-icons/io5";
+
 
 const Navbar = ({ isAddmin }) => {
+    const isSmallScreen = useMediaQuery('(max-width:1024px)');
+const prodileMenuShow = location.pathname.includes('/profile') || location.pathname.includes('/admin/restaurants');
+
     return (
         <nav className="navbar">
             <div className="navbar-container">
                 <div className="navbar-left">
+                    {isSmallScreen && prodileMenuShow && (
+    <IconButton className="menu-button" onClick={handleDrawer}>
+        <IoMenu />
+    </IconButton>
+)}
+
                     <Link to={isAddmin ? '/admin/restaurants' : '/'} className="logo-link">
                         <div className="logo-container">
                             <h1 className="logo-text">
