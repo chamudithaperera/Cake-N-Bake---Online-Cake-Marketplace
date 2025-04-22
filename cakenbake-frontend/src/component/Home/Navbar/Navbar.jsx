35e38a2ec3791c0f229bde1eaf75a0d5e14bb81b
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-04-22 22:58:18 +0530

    feat: enhance Navbar component with logo and navigation links

--- cakenbake-frontend/src/component/Home/Navbar/Navbar.jsx
+++ cakenbake-frontend/src/component/Home/Navbar/Navbar.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ isAddmin }) => {
     return (
         <nav className="navbar">
             <div className="navbar-container">
-                {/* Content to be added in later commits */}
+                <div className="navbar-left">
+                    <Link to={isAddmin ? '/admin/restaurants' : '/'} className="logo-link">
+                        <div className="logo-container">
+                            <h1 className="logo-text">
+                                {!isAddmin ? (
+                                    <>Cake <span className="logo-accent">N</span> Bake</>
+                                ) : (
+                                    "Cake N Bake Admin"
+                                )}
+                            </h1>
+                        </div>
+                    </Link>
+                </div>
             </div>
         </nav>
     );
 };
 
 export default Navbar;
+
