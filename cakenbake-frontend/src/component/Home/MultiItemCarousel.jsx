ed5e99e26d4dac5a20be6217bf9ac75983607260
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-04-23 19:51:39 +0530

    feat: enhance MultiItemCarousel to dynamically display food items with responsive settings

--- cakenbake-frontend/src/component/Home/MultiItemCarousel.jsx
+++ cakenbake-frontend/src/component/Home/MultiItemCarousel.jsx
@@ -1,24 +1,56 @@
 import React from 'react';
 import Slider from 'react-slick';
+import CarouselItem from './CarouselItem';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Home.css";
 
-const MultiItemCarousel = () => {
+const MultiItemCarousel = ({ foods }) => {
     const settings = {
         dots: false,
         infinite: false,
         speed: 500,
-        slidesToShow: 5,
+        slidesToShow: foods?.length || 5,
         slidesToScroll: 1,
         autoplay: false,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: Math.min(foods?.length || 3, 3),
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 980,
+                settings: {
+                    slidesToShow: Math.min(foods?.length || 2, 2),
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     };
 
     return (
         <div className="relative px-2">
             <Slider {...settings}>
-                {/* Items will be rendered here */}
+                {foods?.map((item) => (
+                    <CarouselItem
+                        key={item.id}
+                        image={item.images[0]}
+                        title={item?.name}
+                        price={item?.price}
+                        restaurantId={item?.restaurant?.id}
+                    />
+                ))}
             </Slider>
         </div>
     );
