25573ec2b2682043ecb282686c744f5a12858d13
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-04-23 19:46:59 +0530

    feat: implement MultiItemCarousel with slider settings for future item display

--- cakenbake-frontend/src/component/Home/MultiItemCarousel.jsx
+++ cakenbake-frontend/src/component/Home/MultiItemCarousel.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Home.css";
 
 const MultiItemCarousel = () => {
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false
+    };
+
     return (
         <div className="relative px-2">
-            {/* Slider will go here later */}
+            <Slider {...settings}>
+                {/* Items will be rendered here */}
+            </Slider>
         </div>
     );
 };
