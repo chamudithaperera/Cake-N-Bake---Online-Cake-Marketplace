1b1eb5b5b2b6c30a35211b08b1b03369f35b772f
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-04-23 19:46:16 +0530

    feat: add MultiItemCarousel component for future slider implementation

---
+++ cakenbake-frontend/src/component/Home/MultiItemCarousel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Home.css";
+
+const MultiItemCarousel = () => {
+    return (
+        <div className="relative px-2">
+            {/* Slider will go here later */}
+        </div>
+    );
+};
+
+export default MultiItemCarousel;
