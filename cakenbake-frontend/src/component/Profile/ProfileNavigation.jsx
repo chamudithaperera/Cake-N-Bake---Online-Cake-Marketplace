12532e2a077edbc566368bd2b007fedaec1f704d
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-03 18:00:19 +0530

    feat: add ProfileNavigation component skeleton

---
+++ cakenbake-frontend/src/component/Profile/ProfileNavigation.jsx
@@ -0,0 +1,10 @@
+// ProfileNavigation.jsx
+import React from 'react';
+
+const ProfileNavigation = () => {
+    return (
+        <nav>
+        </nav>
+    );
+};
+export default ProfileNavigation;
