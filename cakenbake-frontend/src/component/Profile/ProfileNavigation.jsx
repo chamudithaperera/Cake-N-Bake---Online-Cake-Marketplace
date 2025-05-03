665d63a64940a4f2b6b2d2d755d454c5acfdc9cf
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-03 18:03:45 +0530

    feat: add navItems and isActive logic to ProfileNavigation

--- cakenbake-frontend/src/component/Profile/ProfileNavigation.jsx
+++ cakenbake-frontend/src/component/Profile/ProfileNavigation.jsx
@@ -1,7 +1,12 @@
 import { NavLink, useLocation } from 'react-router-dom';
 import { Person } from '@mui/icons-material';
-
 import React from 'react';
+
+const location = useLocation();
+const isActive = (path) => location.pathname === `/profile${path}`;
+const navItems = [
+    { path: '', icon: <Person />, label: 'Profile' }
+];
 
 const ProfileNavigation = () => {
     return (
