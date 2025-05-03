efd15629e3efa9826f569ef7349ba5ade8602cba
Pabodha_Wijesinghe <139679251+kushipabo@users.noreply.github.com>
2025-05-03 17:21:39 +0530

    feat: Add dummy user data and logout handler to UserProfile.jsx.

--- cakenbake-frontend/src/component/Profile/UserProfile.jsx
+++ cakenbake-frontend/src/component/Profile/UserProfile.jsx
@@ -3,8 +3,19 @@ import { Avatar, Button } from '@mui/material';
 import { LogoutRounded } from '@mui/icons-material';
 
 const UserProfile = () => {
+
+    const user = {
+        email: 'user@example.com',
+        name: 'John Doe',
+        avatar: '',
+    };
+    const handleLogout = () => {
+        alert('Logged out!');
+    };
+
     return (
         <div>
+            
         </div>
     );
 };
