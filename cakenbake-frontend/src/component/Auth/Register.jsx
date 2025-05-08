7c02318a8accf714084aa5b344369e308cf916c8
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-08 20:48:26 +0530

    Integrate form validation using Yup

--- cakenbake-frontend/src/component/Auth/Register.jsx
+++ cakenbake-frontend/src/component/Auth/Register.jsx
@@ -3,6 +3,7 @@ import { Button, Grid, TextField } from '@mui/material';
 import { Field, Form, Formik } from 'formik';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import * as Yup from 'yup';
 
 const initialValues = {
   fullName: "",
@@ -11,18 +12,35 @@ const initialValues = {
   role: "ROLE_CUSTOMER"
 };
 
+const SignupSchema = Yup.object().shape({
+  fullName: Yup.string().min(2).max(50).required('Name required'),
+  email: Yup.string().email('Invalid email').required('Email required'),
+  password: Yup.string()
+    .required('Password required.')
+    .min(8, 'Too short')
+    .matches(/[a-zA-Z]/, 'Latin letters only.')
+});
+
 const Register = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   return (
     <div>
-      <Formik initialValues={initialValues} onSubmit={() => {}}>
-        {() => (
+      <Formik initialValues={initialValues} onSubmit={() => {}} validationSchema={SignupSchema}>
+        {({ errors, touched }) => (
           <Form>
             <Grid container spacing={2}>
               <Grid item xs={12}>
-                <Field as={TextField} name="fullName" label="Full Name" fullWidth variant="outlined" />
+                <Field
+        as={TextField}
+        name="fullName"
+        label="Full Name"
+        fullWidth
+        variant="outlined"
+        error={touched.fullName && !!errors.fullName}
+        helperText={touched.fullName && errors.fullName}
+      />
               </Grid>
               <Grid item xs={12}>
                 <Field as={TextField} name="email" label="Email" fullWidth variant="outlined" />
