13f9e60cc10ff2bbfef02f25c9e1dfdeccf3af62
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-08 20:37:35 +0530

    Add form structure with Formik and basic layout

--- cakenbake-frontend/src/component/Auth/Register.jsx
+++ cakenbake-frontend/src/component/Auth/Register.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Button, Grid, TextField } from '@mui/material';
+import { Field, Form, Formik } from 'formik';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+
+const initialValues = {
+  fullName: "",
+  email: "",
+  password: "",
+  role: "ROLE_CUSTOMER"
+};
 
 const Register = () => {
   const navigate = useNavigate();
@@ -8,7 +17,23 @@ const Register = () => {
 
   return (
     <div>
-      {/* Register form will go here */}
+      <Formik initialValues={initialValues} onSubmit={() => {}}>
+        {() => (
+          <Form>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Field as={TextField} name="fullName" label="Full Name" fullWidth variant="outlined" />
+              </Grid>
+              <Grid item xs={12}>
+                <Field as={TextField} name="email" label="Email" fullWidth variant="outlined" />
+              </Grid>
+              <Grid item xs={12}>
+                <Field as={TextField} name="password" label="Password" fullWidth variant="outlined" type="password" />
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
