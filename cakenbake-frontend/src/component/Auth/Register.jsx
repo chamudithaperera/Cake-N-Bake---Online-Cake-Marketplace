9c508567e36fef3a1ed90203b86b44fef49f5afe
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-08 20:35:53 +0530

    refactor: improve user profile layout and enhance styling in UserProfile component

---
+++ cakenbake-frontend/src/component/Auth/Register.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+const Register = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      {/* Register form will go here */}
+    </div>
+  );
+};
+
+export default Register;
