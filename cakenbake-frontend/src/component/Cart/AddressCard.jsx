fdf24fdad6118168b7f86a9dacac2ac26a19b037
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-28 20:41:52 +0530

    feat: create AddressCard component to display user addresses

---
+++ cakenbake-frontend/src/component/Cart/AddressCard.jsx
@@ -0,0 +1,20 @@
+import { Button, Card } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import React from 'react';
+
+const AddressCard = ({ item }) => {
+    return (
+        <Card className='flex w-64 gap-5 p-5'>
+            <div className='flex-row '>
+                <div><HomeIcon /></div>
+                <div><h1>{item?.locationType}</h1></div>
+                <div><p>{item?.streetAddress}</p></div>
+                <div><p>{item?.city}</p></div>
+                <div><p>{item?.mobile}</p></div>
+                <div><Button variant='outlined' fullWidth>Select</Button></div>
+            </div>
+        </Card>
+    );
+};
+
+export default AddressCard;
