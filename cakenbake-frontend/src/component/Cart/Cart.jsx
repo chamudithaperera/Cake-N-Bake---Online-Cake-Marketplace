0f756168772adfea20978d7d850ff2c9e8122edb
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-28 21:01:06 +0530

    feat: enhance Cart component with form validation and bill details display

--- cakenbake-frontend/src/component/Cart/Cart.jsx
+++ cakenbake-frontend/src/component/Cart/Cart.jsx
@@ -86,3 +86,57 @@ function Cart() {
             }
         });
     }
+        const initialValues = {
+        location: '',
+        streetAddress: '',
+        mobile: '',
+        city: '',
+    }
+
+    const validationSchema = Yup.object().shape({
+        location: Yup.string().required('Location Type is required'),
+        streetAddress: Yup.string().required('Street Address is required'),
+        mobile: Yup.string()
+            .required('Mobile is required')
+            .matches(/^[0-9]+$/, 'Mobile must be a number')
+            .min(10, 'Mobile must be at least 10 digits')
+            .max(15, 'Mobile must be less than 15 digits'),
+        city: Yup.string().required('City is required'),
+    });
+
+    return (
+        <>
+            <main className='justify-between lg:flex'>
+                <section className='lg:w-[30%] space-y-6 lg:min-h-screen pt-10'>
+                    {cart?.cartItems.length > 0 ? (
+                        cart.cartItems?.map(item => <CartItem key={item.id} item={item} />)
+                    ) : (
+                        <div className='px-20'>
+                            <div className='flex justify-center'>
+                                <Button onClick={handleAddItemsNavigation} fullWidth variant='outlined'>
+                                    Add Items
+                                </Button>
+                            </div>
+                        </div>
+                    )}
+                    <Divider />
+                    <div className='px-5 text-sm billDetails'>
+                        <p className='py-5 font-extralight'>Bill Details</p>
+                        <div className='pb-3 space-y-3'>
+                            <div className='flex justify-between text-gray-400'>
+                                <p>Item Total</p>
+                                <p>Rs. {cart?.cart?.total}</p>
+                            </div>
+                            <div className='flex justify-between text-gray-400'>
+                                <p>Delivery Free</p>
+                                <p>Rs. 00</p>
+                            </div>
+                        </div>
+                        <Divider />
+                        <div className='flex justify-between text-gray-400'>
+                            <p>Total pay</p>
+                            <p>Rs. {cart?.cart?.total}</p>
+                        </div>
+                    </div>
+                </section>
+
