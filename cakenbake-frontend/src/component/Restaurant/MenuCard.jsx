4c979635d9895a9a28b0adb7a44ed93bc46a9c16
Pabodha_Wijesinghe <139679251+kushipabo@users.noreply.github.com>
2025-05-29 10:59:53 +0530

    feat: implement ingredient selection and add-to-cart dispatch functionality

--- cakenbake-frontend/src/component/Restaurant/MenuCard.jsx
+++ cakenbake-frontend/src/component/Restaurant/MenuCard.jsx
@@ -1,8 +1,47 @@
-import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import { Accordion, Button, Checkbox, FormControlLabel, FormGroup } from '@mui/material'
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import React from 'react';
+import React, { useState } from 'react'
+import { categorizeIngrdients } from '../../utils/categrizeingredinets';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart, getAllCartItems } from '../State/Cart/Action';
+
 
 const MenuCard = ({ item }) => {
+
+    const [selectedIngrdients, setSelectedIngredinets] = useState([]);
+    const dispatch = useDispatch();
+    const jwt = localStorage.getItem("jwt");
+
+    const handleAddItemToCart = (e) => {
+
+        e.preventDefault();
+        const reqData = {
+            token: localStorage.getItem("jwt"),
+            cartItem: {
+                foodId: item.id,
+                quantity: 1,
+                ingredients: selectedIngrdients
+            }
+        };
+
+        dispatch(addItemToCart(reqData));
+
+        //naviagte("/cart");
+    }
+
+
+    const handleCheckBoxChange = (itemName) => {
+
+        if (selectedIngrdients.includes(itemName)) {
+
+            setSelectedIngredinets(selectedIngrdients.filter(item => item !== itemName));
+        } else {
+            setSelectedIngredinets([...selectedIngrdients, itemName]);
+        }
+    }
+    
     return (
         <div>
             <Accordion>
@@ -12,22 +51,48 @@ const MenuCard = ({ item }) => {
                     id="panel1-header"
                 >
                     <div className='items-center justify-between lg:flex lg:gap-5'>
-                        <div className='items-center lg:flex'>
+                        <div className='items-center lg:flex ' >
                             <img className='w-[7rem] h-[7rem] object-cover' src={item.images[0]} alt="" />
                         </div>
                         <div className='space-y-1 lg:space-y-5 lg:max-w-2xl'>
                             <p className='text-xl font-semibold'>{item.name}</p>
-                            <p>{item.price} LKR</p>
+                            <p >{item.price} LKR</p>
                             <p className='text-gray-400'>{item.description}</p>
                         </div>
                     </div>
                 </AccordionSummary>
                 <AccordionDetails>
-                    {/* Future form for cart interaction */}
+                    <form onSubmit={handleAddItemToCart}>
+
+                        <div className='flex flex-wrap gap-5'>
+                            {
+                                Object.keys(categorizeIngrdients(item.ingredients)).map(category =>
+                                    <div>
+                                        <p>{category}</p>
+                                        <FormGroup>
+
+                                            {
+                                                categorizeIngrdients(item.ingredients)[category].map(item =>
+
+                                                    <FormControlLabel key={item.id} control={<Checkbox disabled={!item.inStoke} onChange={() => handleCheckBoxChange(item.name)} />} label={item.name} />
+                                                )
+                                            }
+
+                                        </FormGroup>
+                                    </div>
+
+                                )
+                            }
+                        </div>
+
+                        <div className='pt-5'>
+                            <Button type="submit" variant='contained' disabled={!item.available}>{item.available  ? ("Add to Cart") : ("Out of Stock")}</Button>
+                        </div>
+                    </form>
                 </AccordionDetails>
             </Accordion>
         </div>
-    );
-};
+    )
+}
 
-export default MenuCard;
+export default MenuCard
