13ea8fd7d1fab363185c85b7d8e147c83c3e78a8
Pabodha_Wijesinghe <139679251+kushipabo@users.noreply.github.com>
2025-05-31 15:41:59 +0530

    feat: implement reducer logic for restaurant CRUD operations and event management

--- cakenbake-frontend/src/component/State/Restaurant/Reducer.jsx
+++ cakenbake-frontend/src/component/State/Restaurant/Reducer.jsx
@@ -12,3 +12,95 @@ const initialState = {
     categories: [],
     foods: []
 }
+
+const restaurantReducer = (state = initialState, action) => {
+    switch (action.type) {
+        // Action Types handled in the reducer:
+case actionTypes.CREATE_RESTAURANT_REQUEST:
+case actionTypes.GET_ALL_RESTAURANTS_REQUEST:
+case actionTypes.DELETE_RESTAURANT_REQUEST:
+case actionTypes.UPDATE_RESTAURANT_REQUEST:
+case actionTypes.GET_RESTAURANT_BY_ID_REQUEST:
+
+// Restaurant Success cases:
+case actionTypes.CREATE_RESTAURANT_SUCCESS:
+    return {
+        ...state,
+        loading: false,
+        usersRestaurant: action.payload
+    }
+
+case actionTypes.GET_ALL_RESTAURANTS_SUCCESS:
+    return {
+        ...state,
+        loading: false,
+        restaurants: action.payload
+    }
+
+case actionTypes.GET_RESTAURANT_BY_ID_SUCCESS:
+    return {
+        ...state,
+        loading: false,
+        restaurant: action.payload
+    }
+
+case actionTypes.GET_RESTAURANT_BY_USER_ID_SUCCESS:
+case actionTypes.UPDATE_RESTAURANT_STATUS_SUCCESS:
+case actionTypes.UPDATE_RESTAURANT_SUCCESS:
+    return {
+        ...state,
+        loading: false,
+        usersRestaurant: action.payload
+    }
+
+case actionTypes.DELETE_RESTAURANT_SUCCESS:
+    return {
+        ...state,
+        error: null,
+        loading: false,
+        restaurants: state.restaurants.filter(
+            (item) => item.id !== action.payload
+        ),
+        usersRestaurnat:null
+    }
+
+// Event creation
+case actionTypes.CREATE_EVENTS_SUCCESS:
+    return {
+        ...state,
+        loading: false,
+        events: [...state.events, action.payload],
+        restaurantsEvents: [...state.restaurantsEvents, action.payload]
+    }
+
+case actionTypes.GET_ALL_EVENTS_SUCCESS:
+case actionTypes.GET_RESTAURANTS_EVENTS_SUCCESS:
+    return {
+        ...state,
+        loading: false,
+        restaurantsEvents: action.payload
+    }
+
+case actionTypes.GET_RESTAURANT_EVENTS_SUCCESS_BY_ID:
+    return {
+        ...state,
+        loading: false,
+        events: action.payload
+    }
+
+case actionTypes.DELETE_EVENTS_SUCCESS:
+    return {
+        ...state,
+        loading: false,
+        events: state.events.filter(
+            (item) => item.id !== action.payload
+        ),
+        restaurantsEvents: state.restaurantsEvents.filter(
+            (item) => item.id !== action.payload
+        )
+    }
+
+    }
+}
+
+export default restaurantReducer;
