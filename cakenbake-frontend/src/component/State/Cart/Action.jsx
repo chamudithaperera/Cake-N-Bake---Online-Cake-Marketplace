e6c50df23238e46cab5e5c905f8b84ad8851f064
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-06-15 16:30:46 +0530

    feat: add getAllCartItems async action

--- cakenbake-frontend/src/component/State/Cart/Action.jsx
+++ cakenbake-frontend/src/component/State/Cart/Action.jsx
@@ -16,3 +16,23 @@ export const findCart = (token) => {
         }
     }
 }
+
+export const getAllCartItems = (reqData) => {
+    return async (dispach) => {
+        dispach({ type: GET_ALL_CART_ITEMS_REQUEST });
+
+        try {
+            const response = await api.get(`/api/carts/${reqData.cartId}/items`, {
+                headers: {
+                    Authorization: `Bearer ${reqData.token}`
+                }
+            });
+
+            dispach({ type: GET_ALL_CART_ITEMS_SUCCESS, payload: response.data });
+            console.log("All Cart Items", response.data);
+        } catch (error) {
+            console.log(error);
+            dispach({ type: GET_ALL_CART_ITEMS_FAILURE, payload: error });
+        }
+    }
+}
