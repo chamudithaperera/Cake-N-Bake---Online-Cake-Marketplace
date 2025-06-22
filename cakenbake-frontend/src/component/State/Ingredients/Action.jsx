c285cec460b8e119b81d7ef204645cc54d7af74f
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-06-22 19:53:36 +0530

    feat: add createIngredientCategory, getIngredientCategory, and updateStockOfIngredient actions with API integration and user feedback

--- cakenbake-frontend/src/component/State/Ingredients/Action.jsx
+++ cakenbake-frontend/src/component/State/Ingredients/Action.jsx
@@ -59,3 +59,93 @@ export const createIngredient = ({ data, jwt }) => {
     }
 
 }
+
+export const createIngredientategory = ({ data, jwt }) => {
+
+    return async (dispach) => {
+
+        dispach({ type: CREATE_INGREDIENT_CATEGORY_REQUEST });
+
+        try {
+
+            const response = await api.post(`/api/admin/ingredients/category`, data, {
+
+                headers: {
+                    Authorization: `Bearer ${jwt}`
+                }
+            }
+            );
+            await Swal.fire({
+                title: "create category succesfully",
+                position: "center",
+               timer:1500,
+               showConfirmButton:false,
+                icon: "success"
+              });
+            dispach({ type: CREATE_INGREDIENT_CATEGORY_SUCCESS, payload: response.data });
+            console.log("Create ingredient category", data);
+        } catch (error) {
+            dispach({ type: CREATE_INGREDIENT_CATEGORY_FAILURE, payload: error });
+            console.log(error);
+        }
+    }
+
+}
+
+export const getIngredientCategory = ({ id, jwt }) => {
+
+    return async (dispach) => {
+
+        dispach({ type: GET_INGREDIENT_CATEGORY_REQUEST });
+
+        try {
+
+            const response = await api.get(`/api/admin/ingredients/restaurant/${id}/category`, {
+
+                headers: {
+                    Authorization: `Bearer ${jwt}`
+                }
+            }
+            );
+
+            dispach({ type: GET_INGREDIENT_CATEGORY_SUCCESS, payload: response.data });
+            console.log("Get ingredient category", response.data);
+        } catch (error) {
+            dispach({ type: GET_INGREDIENT_CATEGORY_FAILURE, payload: error });
+            console.log(error);
+        }
+    }
+
+}
+
+
+export const updateStockOfIngredient = ({ id, jwt }) => {
+
+    return async (dispach) => {
+
+
+
+        try {
+
+            const { data } = await api.put(`/api/admin/ingredients/${id}/stock`, {}, {
+
+                headers: {
+                    Authorization: `Bearer ${jwt}`
+                }
+            }
+            );
+            await Swal.fire({
+                title: "update stock succesfully",
+               timer:1500,
+               position: "center",
+               showConfirmButton:false,
+                icon: "success"
+              });
+            dispach({ type: UPDATE_STOCKS, payload: data });
+            console.log("Update stocks", data)
+        } catch (error) {
+            dispach(error);
+        }
+    }
+
+}
